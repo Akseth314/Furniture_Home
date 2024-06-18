19626ab0f8e98bab87a2b92491fb6ea9723d8e46
Akseth314 <akankshaseth314@gmail.com>
2024-06-18 21:46:05 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const reviews = [
+   {
+    id: 1,
+    name:"Abir Pal",
+    image:"https://wallpapercave.com/wp/wp2352480.jpg",
+    job: "Graphic Designer",
+    text:"I have been working with this company for several years.A graphic designer creates visual concepts using software or by hand to communicate ideas that inspire, inform, or captivate consumers."
+   },
+   {
+    id: 2,
+    name:"Soumya Banerjee",
+    image:"https://wallpapercave.com/wp/wp6893093.jpg",
+    job: "Marketing Manager",
+    text:"I am thrilled with the results of our recent marketing camaign. Marketing management involves planning, executing, and analyzing strategies to meet consumer needs and achieve organizational objectives effectively and efficiently. "
+   },
+   {
+    id: 3,
+    name:"Saikat Mukherjee",
+    image:"https://thumbs.dreamstime.com/z/handsome-young-man-harmony-beautiful-natural-emotions-gorgeous-outdoors-leaning-against-wall-near-green-plants-italian-67254602.jpg",
+    job: "Content Strategist",
+    text:"I have been working with my content creaters over the years.Content strategies involve planning, creating, distributing, and managing content to engage, inform, and attract audiences towards achieving business goals effectively."
+   },
+   {
+    id: 4,
+    name:"Aritra baisal",
+    image:"https://wallpapersmug.com/download/1366x768/970117/portrait-girl-model-autumn.jpg",
+    job: "Data analyst",
+    text:"I have been working with this company for several years Data analysis involves examining, interpreting, and deriving insights from datasets to inform decision-making and solve problems effectively."
+   },
+   {
+    id: 5,
+    name:"Abir Pal",
+    image:"https://4.bp.blogspot.com/-pVLtwBOoo-c/UdexK5eao1I/AAAAAAAAOSY/QXLC1XZgmqA/s1600/tumblr_mnzgrpXEeO1rr8br7o3_1280.png",
+    job: "Graphic Designer",
+    text:"I have been working with this company for several years.A graphic designer creates visual concepts using software or by hand to communicate ideas that inspire, inform, or captivate consumers."
+   },
+];
+
+export default reviews;
